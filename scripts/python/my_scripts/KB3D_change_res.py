324035f5b5fd9ffed1447b757fe252f683e32fd1
Ahmed Hindy <ahmed.hindy96@gmail.com>
2024-05-22 18:00:26 +0300

    principled shader -> Arnold converter is more robust. will adjust rest of the materials

--- scripts/python/my_scripts/KB3D_change_res.py
+++ scripts/python/my_scripts/KB3D_change_res.py
@@ -1,35 +1,48 @@
 import hou
 
 origVar = "4k"  # orig string to replace
-newVar = """`chs("../../res")`"""  # replace with this
+newVar  = """`chs("../../res")`"""  # replace with this
+obj     = hou.node("/obj")
 
-obj = hou.node("/obj")
-nodes = hou.selectedNodes()
+def add_str_res_parm_to_node(node, param_name='parm_name', label='parm_label', default_value="4k") -> None:
+    """
+    function: adds a string parameter for a node.
+    need to make sure the input is a valid node object and not a list, currently this is hardcoded in the next 3 lines,
+    maybe a seperate validator function?
+    """
+    print(f'//{node.type().name()=}')
+    if node.type().name() != 'matnet':
+        raise ValueError("Invalid hou.Node object provided.")
+
+    # Create a string parameter template with a default value
+    parm_template = hou.StringParmTemplate(param_name, label, 1, default_value=[default_value])
+    # Get the existing parameter group from the node
+    parm_group = node.parmTemplateGroup()
+    # Append the new parameter template to the group
+    parm_group.append(parm_template)
+    # Update the node with the new parameter group
+    node.setParmTemplateGroup(parm_group)
 
 
-# matnet = hou.node('/obj/KB3D_EveryCityEmergencyResponse/matnet') #I disabled it but didnt test it yet, enable if problematic
-
-
-def getPrincipledShaders(kwargs):
+def getPrincipledShaders(kwargs, node):
     principledShadersList = []
-    for matnet in nodes:
-        matnetChildren = matnet.children()
-        for child in matnetChildren:
-            childTypeName = child.type().name()
-            if childTypeName == "materialbuilder":
-                # print(f"material builder nodes found in {matnet.name()} and it is called: {child.name()}")
-                materialbuilderChildren = child.children()
-                for materialbuilderChild in materialbuilderChildren:
-                    if materialbuilderChild.type().name() == "principledshader::2.0":
-                        # print(f"I found a principled shader called {materialbuilderChild.name()}")
-                        principledShader = materialbuilderChild
-                        principledShadersList.append(principledShader)
-                        # print(f"principledShadersList: {principledShadersList}")
+    matnetChildren = node.children()
+    for child in matnetChildren:
+        childTypeName = child.type().name()
+        if childTypeName == "materialbuilder":
+            # print(f"material builder nodes found in {matnet.name()} and it is called: {child.name()}")
+            materialbuilderChildren = child.children()
+            for materialbuilderChild in materialbuilderChildren:
+                if materialbuilderChild.type().name() == "principledshader::2.0":
+                    # print(f"I found a principled shader called {materialbuilderChild.name()}")
+                    principledShader = materialbuilderChild
+                    principledShadersList.append(principledShader)
+                    # print(f"principledShadersList: {principledShadersList}")
     return principledShadersList
 
 
-def changeRes(kwargs):
-    for principledShader in getPrincipledShaders(kwargs):
+def changeRes(kwargs, nodes):
+    for principledShader in getPrincipledShaders(kwargs, nodes):
         ### for each principled shader found, lets check the parameters if they contain "_texture"  ###
         parametersAll = principledShader.parms()
         parameterTexture = []
@@ -42,18 +55,18 @@ def changeRes(kwargs):
         ###  we will now replace the origVar with the "newVar" ###
         for x in parameterTexture:
             origString = x.unexpandedString()
-            print(f"origString before was {origString}")
-            print(f"newvar is {newVar} and its type is")
+            # print(f"origString before was {origString}")
+            # print(f"newvar is {newVar} and its type is")
             if origVar in origString:
-                print(f"origVar is {origVar}")
+                # print(f"origVar is {origVar}")
                 origString = origString.replace(origVar, newVar)
-                print(f"origString after is {origString}")
+                # print(f"origString after is {origString}")
                 x.set(origString)
                 # print("new String is: " + newString)
 
 
-def changeRough(kwargs):
-    for principledShader in getPrincipledShaders(kwargs):
+def changeRough(kwargs, nodes):
+    for principledShader in getPrincipledShaders(kwargs, nodes):
         # print(f"principledShader: {principledShader}")
         parametersAll = principledShader.parms()
         principledShader.parm("rough").set(1)
