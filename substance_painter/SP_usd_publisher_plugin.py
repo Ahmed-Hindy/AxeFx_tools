206a489f1ec62862add378966e6bef926dd2b140
Ahmed Hindy <Ahmed.hindy96@gmail.com>
2024-06-23 23:24:19 +0300

    SP_usd_publisher_plugin.py now has export location QLineEdit that the user can edit.

--- substance_painter/SP_usd_publisher_plugin.py
+++ substance_painter/SP_usd_publisher_plugin.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from importlib import reload
 from pprint import pprint
 from typing import Dict, Tuple, List
@@ -22,7 +23,7 @@ import substance_painter.event
 import substance_painter.project
 import substance_painter.export
 import substance_painter.resource
-from PySide2.QtWidgets import QDialog, QVBoxLayout, QCheckBox, QGridLayout, QLabel
+from PySide2.QtWidgets import QDialog, QVBoxLayout, QHBoxLayout, QCheckBox, QGridLayout, QLabel, QLineEdit, QSpacerItem, QSizePolicy
 from PySide2.QtCore import Qt
 
 
@@ -78,17 +79,27 @@ def close_plugin():
 
 
 class CreateUSD:
-    def __init__(self, view, textures_dict=None):
-        self.view = view
+    def __init__(self, usd_exported_qdialog, textures_dict=None):
+        self.usd_exported_qdialog = usd_exported_qdialog
         self.textures_dict = textures_dict
+        self.textures_publish_dir = self.extract_textures_publish_location()
 
-        self.json_tmp_file = r"F:\Users\Ahmed Hindy\Documents\Adobe\Adobe Substance 3D Painter\export\usd_export_tests\log.json"
-        self.export_folder = r"F:\Users\Ahmed Hindy\Documents\Adobe\Adobe Substance 3D Painter\export\usd_export_tests"
-        self.usd_file = f"{self.export_folder}/material_scene.usda"
+        self.selected_options_dict = self.usd_exported_qdialog.get_selected_options()
+        self.checkbox_create_usdpreview = self.selected_options_dict['usdpreview']
+        self.checkbox_create_arnold = self.selected_options_dict['arnold']
+        self.checkbox_create_materialx = self.selected_options_dict['materialx']
+        self.usd_publish_location_with_token = self.selected_options_dict['publish_location']  # has <export_folder> token
+
+        self.usd_publish_location = self.get_usd_publish_path()  # replace the <export_folder> token
         self.project = substance_painter.project
         self.export_settings = None
         self.stage = None
         self.material_parent_path = '/RootNode/material_2'  # TODO: temp, add it as a Qlabel to the pyside QDialog.
+
+    def get_usd_publish_path(self):
+        """Replace <export_folder> in usd_file_template with the textures_publish_dir"""
+        publish_location = self.usd_publish_location_with_token.replace('<export_folder>', self.textures_publish_dir)
+        return publish_location
 
     def extract_material_names(self, textures_dict) -> List[str]:
         """
@@ -112,6 +123,16 @@ class CreateUSD:
                 return texture_list
         return []
 
+    def extract_textures_publish_location(self) -> str:
+        """
+        Extract textures publish folder from textures_dict
+        :return: string folder path for the first material encountered in the for loop
+        """
+        for (_, _), texture_list in self.textures_dict.items():
+            textures_publish_dir = os.path.dirname(texture_list[0])
+            textures_parent_directory = os.path.dirname(textures_publish_dir)
+            return textures_parent_directory
+
     def convert_keys_to_strings(self, textures_dict: Dict[Tuple[str, str], List[str]]):
         """[tmp] for json dumping only!, Converts tuple keys to strings"""
         if isinstance(textures_dict, dict):
@@ -121,40 +142,18 @@ class CreateUSD:
         else:
             return textures_dict
 
-    def get_export_template_info(self):
-        """Get current export template information"""
-
-        if not self.export_settings:
-            print("No export settings found.")
-            return
-
-        print("Export Settings Information:")
-        pprint(self.export_settings)
-
-        # Export template
-        template = self.export_settings.get('template')
-        print(f"Export Template: {template}")
-
-        # Get texture maps info
-        texture_maps_info = self.export_settings.get('maps')
-        for map_info in texture_maps_info:
-            print(f"Map Name: {map_info['name']}")
-            print(f"  Channel: {map_info['channel']}")
-            print(f"  Format: {map_info['format']}")
-            print(f"  Bit Depth: {map_info['bit_depth']}")
-            print(f"  Naming Convention: {map_info['naming']}")
-            print(f"  Selected: {map_info['selected']}")
-
     def create_materialdata(self) -> List[MaterialData]:
-        """Create a MaterialData object from exported texture data"""
+        """
+        Create a MaterialData object from exported texture data.
+        takes in self.textures_dict: Dict[Tuple[str, str], List[str]]
+        :return: list of MaterialData object for all materials.
+        """
         materialdata_list = []
         for (material_name, stack_name), texture_paths_list in self.textures_dict.items():
             material_name = material_name.replace(' ', '_')
             stack_name = stack_name.replace(' ', '_')
             material_path = f"{self.material_parent_path}/{material_name}"
             print(f"\n  ///{material_name=}, {material_path=}")
-
-            # usd_material = UsdShade.Material.Define(self.stage, material_path)
 
             normalized_dict = {}
             for tex_path in texture_paths_list:
@@ -184,25 +183,18 @@ class CreateUSD:
             print(f"Created MaterialData: {material_data}")
         return materialdata_list
 
-    def write_material_to_usd(self, material_data):
+    def write_material_to_usd(self, material_data) -> None:
         """Write MaterialData to a USD file using USD_Shader_Create"""
-        # get checkboxes for material creation:
-        selected_options = self.view.get_selected_options()
-        self.create_usdpreview = selected_options['usdpreview']
-        self.create_arnold = selected_options['arnold']
-        self.create_materialx = selected_options['materialx']
-        # print(f"/////{selected_options=}")
-
         USD_Shader_Create(self.stage, material_data, parent_prim=self.material_parent_path,
-                          create_usd_preview=self.create_usdpreview, create_arnold=self.create_arnold,
-                          create_mtlx=self.create_materialx)
+                          create_usd_preview=self.checkbox_create_usdpreview, create_arnold=self.checkbox_create_arnold,
+                          create_mtlx=self.checkbox_create_materialx)
         self.stage.GetRootLayer().Save()
 
-        print(f"Material USD file has been exported to {self.usd_file}")
+        print(f"Material USD file has been exported to {self.usd_publish_location}")
 
     def run(self):
         print("exporting usd....")
-        self.stage = Usd.Stage.CreateNew(self.usd_file)
+        self.stage = Usd.Stage.CreateNew(self.usd_publish_location)
         materialdata_list = self.create_materialdata()
         for materialdata in materialdata_list:
             self.write_material_to_usd(materialdata)
@@ -224,15 +216,29 @@ class USDExporterView(QDialog):
 
         self.usdpreview_checkbox = QCheckBox("USD Preview")
         self.arnold_checkbox = QCheckBox("Arnold")
-        self.materialx_checkbox = QCheckBox("MaterialX")
+        self.materialx_checkbox = QCheckBox("MaterialX [WIP]")
         self.usdpreview_checkbox.setChecked(True)
         self.arnold_checkbox.setChecked(True)
 
-        self.label_renderers = QLabel("Renderers         ")
+        self.label_renderers = QLabel("Renderers             ")
         self.material_options_layout.addWidget(self.label_renderers, 0, 0)
         self.material_options_layout.addWidget(self.usdpreview_checkbox, 0, 1)
         self.material_options_layout.addWidget(self.arnold_checkbox, 1, 1)
         self.material_options_layout.addWidget(self.materialx_checkbox, 2, 1)
+
+        # Material Publish Location
+        self.publish_location_vbox = QHBoxLayout()
+        self.publish_location_vbox.setAlignment(Qt.AlignTop | Qt.AlignLeft)
+        self.publish_location_label = QLabel("Publish Location ")
+        self.publish_location_lineedit = QLineEdit("<export_folder>/materials.usda")
+        self.publish_location_lineedit.setMaxLength(128)
+        self.publish_location_lineedit.setMinimumWidth(300)
+        self.publish_location_lineedit.setMaximumWidth(600)
+        self.publish_location_lineedit.setSizePolicy(QSizePolicy.Policy.MinimumExpanding, QSizePolicy.Policy.Fixed)
+        self.publish_location_spacer = QSpacerItem(0, 0, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed)
+        self.publish_location_vbox.addWidget(self.publish_location_label)
+        self.publish_location_vbox.addWidget(self.publish_location_lineedit)
+        self.publish_location_vbox.addItem(self.publish_location_spacer)
 
         # Save geometry option
         self.save_geometry_checkbox = QCheckBox("Save Geometry in USD File")
@@ -240,6 +246,7 @@ class USDExporterView(QDialog):
         # add everything to the main layout
         self.outer_layout.addWidget(self.select_engines_label)
         self.outer_layout.addLayout(self.material_options_layout)
+        self.outer_layout.addLayout(self.publish_location_vbox)
         self.outer_layout.addWidget(self.save_geometry_checkbox)
 
 
@@ -249,6 +256,7 @@ class USDExporterView(QDialog):
             "usdpreview": self.usdpreview_checkbox.isChecked(),
             "arnold": self.arnold_checkbox.isChecked(),
             "materialx": self.materialx_checkbox.isChecked(),
+            "publish_location": self.publish_location_lineedit.text(),
             "save_geometry": self.save_geometry_checkbox.isChecked()
         }
 
