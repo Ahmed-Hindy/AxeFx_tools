8191f4ae9cba6325b2b22d3d837fc71433453331
Ahmed Hindy <Ahmed.hindy96@gmail.com>
2024-07-02 20:35:53 +0300

    New traversal method WIP. currently it doesn't work and require alot of refactoring, so I am moving it to this new branch.

--- scripts/python/Material_Processor/material_classes.py
+++ scripts/python/Material_Processor/material_classes.py
@@ -1,27 +1,28 @@
 from dataclasses import dataclass, field
 from typing import Optional, Dict, List
 
-from pxr import UsdShade, Usd
+@dataclass
+class NodeInfo:
+    node_type: str
+    traversal_path: str
+    connected_input_index: Optional[int] = None
 
+    def __str__(self):
+        return f"NodeInfo(node_type={self.node_type}, traversal_path={self.traversal_path}, connected_input_index={self.connected_input_index})"
 
 @dataclass
 class TextureInfo:
     file_path: str
-    traversal_path: str
-    connected_input: Optional[str] = None
+    nodes: List[NodeInfo] = field(default_factory=list)
 
     def __str__(self):
-        return f"TextureInfo(file_path={self.file_path}, traversal_path={self.traversal_path}, connected_input={self.connected_input})"
-
+        return f"TextureInfo(file_path={self.file_path}, nodes={self.nodes})"
 
 @dataclass
 class MaterialData:
     material_name: str
     material_path: Optional[str] = None
-    usd_material: Optional[UsdShade.Material] = None
-    textures: Dict[str, TextureInfo] = field(default_factory=dict)  # e.g. {'albedo:TextureInfo(file_path='F:\\unsplash.jpg', traversal_path='', connected_input='')}
-    prims_assigned_to_material: List[Usd.Prim] = field(default_factory=list)
-
+    textures: Dict[str, TextureInfo] = field(default_factory=dict)
 
     def __str__(self):
         return self._pretty_print()
@@ -30,6 +31,4 @@ class MaterialData:
         return self._pretty_print()
 
     def _pretty_print(self):
-        return f"MaterialData(prim_path={self.material_path})"
-        # data_dict = asdict(self)
-        # return pformat(data_dict, indent=4)
+        return f"MaterialData(material_name={self.material_name}, textures={self.textures})"
